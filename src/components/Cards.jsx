4d6ceef6cdece1f0621794bb7130c20bbe936ebc
Mridhul <mridhul67@gmail.com>
2025-03-05 22:23:29 +0530

    Feat: Implement function to randomly generate 6 pokemons and display their sprites

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,24 +2,53 @@ import "../styles/style.css";
 import "../styles/modern-normalize.css";
 
 export default function Cards() {
+  let selectedPokemons = [];
+
+  const selectPokemons = () => {
+    let iterator = 0;
+    while (iterator < 6) {
+      const randomNum = Math.floor(Math.random() * 150) + 1;
+      selectedPokemons.push(randomNum);
+      iterator += 1;
+    }
+  };
+
+  selectPokemons();
+
   async function getSprite() {
-    const pokeData = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
-    const pokeJson = await pokeData.json();
-    const imgUrl = pokeJson.sprites.front_default;
-    const imageCont = document.querySelector(".img-cont");
-    imageCont.src = imgUrl;
+    let iterator = 0;
+    while (iterator < 6) {
+      const pokeData = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${selectedPokemons[iterator]}/`
+      );
+      const pokeJson = await pokeData.json();
+      const imgUrl = pokeJson.sprites.front_default;
+      const imageCont = document.querySelector(`.img-cont-${iterator + 1}`);
+      imageCont.src = imgUrl;
+      iterator += 1;
+    }
   }
   getSprite();
   return (
     <div className="cards-container">
       <div className="card-1 card">
-        <img src="" alt="pik" className="img-cont" />
+        <img src="" alt="pik" className="img-cont-1" />
       </div>
-      <div className="card-2 card"></div>
-      <div className="card-3 card"></div>
-      <div className="card-4 card"></div>
-      <div className="card-5 card"></div>
-      <div className="card-6 card"></div>
+      <div className="card-2 card">
+        <img src="" alt="pik" className="img-cont-2" />
+      </div>
+      <div className="card-3 card">
+        <img src="" alt="pik" className="img-cont-3" />
+      </div>
+      <div className="card-4 card">
+        <img src="" alt="pik" className="img-cont-4" />
+      </div>
+      <div className="card-5 card">
+        <img src="" alt="pik" className="img-cont-5" />
+      </div>
+      <div className="card-6 card">
+        <img src="" alt="pik" className="img-cont-6" />
+      </div>
     </div>
   );
 }
